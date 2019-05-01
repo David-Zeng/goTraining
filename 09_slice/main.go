ca2f75a1fc613b008a61bef4e781c169d82a45f0
David Zeng <david.zengdj@gmail.com>
2019-05-01 15:49:06 +1000

    modified 09_slice

--- 09_slice/main.go
+++ 09_slice/main.go
@@ -13,10 +13,12 @@ func main() {
 	fmt.Println(s)
 	fmt.Println(len(s))
 	fmt.Println(s[0:len(s)])
+	fmt.Printf("%T", s)
 
 	var s1 []int
 	s1 = append(s1, 1, 2, 3)
 	fmt.Println(s1)
+	fmt.Printf("%T", s1)
 
 	for i, v := range s {
 		fmt.Println(i)
@@ -31,7 +33,15 @@ func main() {
 		fmt.Println(j)
 		fmt.Printf("s slice value: %v\n", s[j])
 	}
+	cs := append(s[:3], s[4:]...)
+	fmt.Println(cs)
 
+	intSliceDel(s1, 3)
+	intSliceDel(s, 3)
+}
 
-
+func intSliceDel(inSlice []int, i int) []int {
+	fmt.Printf("orig slice: %v\n", inSlice)
+	fmt.Printf("changed slice: %v\n", append(inSlice[:i], inSlice[i+1:]...))
+	return append(inSlice[:i], inSlice[i+1:]...)
 }
