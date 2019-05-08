2f544ae407695636c5aaef60b2faa321f54ac93d
David Zeng <david.zengdj@gmail.com>
2019-05-08 22:30:48 +1000

    work on function

--- 12_function1/main.go
+++ 12_function1/main.go
@@ -12,13 +12,35 @@ func main() {
 
 	doSomething()
 
+	fmt.Println(IntSumFunc(1,2,3,4,5,6,7,8,9))
+
 	fmt.Println(getFactorial(5))
+
 }
 
 // simple function
 func doSomething() {
 	fmt.Println("do something")
 }
+
+// sum of ints
+func IntSumFunc(nums ...int) int {
+	fmt.Printf("%T\n", nums)
+	sum := 0
+	for _, v := range nums {
+		sum += v
+	}
+	return sum
+}
+
+// greeting func
+func Greeting(prefix string, who ...string) {
+
+}
+
+Greet
+
+
 
 // recursive
 // n! = n×(n-1)! where n >0
@@ -29,3 +51,4 @@ func getFactorial(num int) int {
 		return 1 // 1! == 1
 	}
 }
+
